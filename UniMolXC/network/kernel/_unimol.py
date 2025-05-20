e93e106eb8d6ec6d8c7e10357fb25ac16f61577f
kirk0830 <yike.huang@aliyun.com>
2025-05-20 18:06:04 +0800

    Feature: extensive support on UniMol training parameters

--- UniMolXC/network/kernel/_unimol.py
+++ UniMolXC/network/kernel/_unimol.py
@@ -63,14 +63,58 @@ class UniMolRegressionNet:
               metrics='mse',
               epochs=10,
               batch_size=16,
-              save_path=None):
+              save_path=None,
+              **kwargs):
         '''
         trigger the training process of the UniMol multilabel 
-        regression model.
+        regression model. We strongly recommend users to assign
+        appropriate values to the parameters `epochs`, `batch_size`
+        and `metrics` to get a good model and do not let them
+        to be the default values. Other parameters are optional
+        and will also be passed to the training process.
+        
+        Parameters
+        ----------
+        data : dict or list of str
+            the training data. For the use in this package, please
+            refer to the function in file network/train.py:
+            build_dataset_from_abacus.
+        metrics : str, optional
+            the way to scalarize the loss. Default is 'mse', which
+            supports both scalar and vector labels. To see other
+            options, please see annotation of the function
+            unimol_tools.MolTrain.__init__.
+        epochs : int, optional
+            the number of epochs to train. Default is 10.
+        batch_size : int, optional
+            the batch size of training. Default is 16.
+        save_path : str, optional
+            the path to save the model. Default is None, which means
+            the model will not be saved. If you want to save the model,
+            please provide a valid path.
+        **kwargs : dict
+            other parameters to be passed to the training process.
+            For the full list of parameters, please refer to the
+            function unimol_tools.MolTrain.__init__.
         '''
         # distinct with the original UniMol, we force the user
         # to provide the save_path for the model to be saved.
         assert save_path is not None
+        
+        # we check the parameters list of the constructor of
+        # unimol_tools.MolTrain, and we only keep the parameters
+        # closely related to the training process.
+        unimol_mol_train_instantiate_selected_kwargs = [
+            'learning_rate', 'early_stopping', 'split', 
+            'split_group_col', 'kfold', 'remove_hs', 'smiles_col', 
+            'target_cols', 'target_col_prefix', 'target_anomaly_check', 
+            'smiles_check', 'target_normalize', 'max_norm', 'use_cuda', 
+            'use_amp', 'use_ddp', 'use_gpu', 'freeze_layers', 
+            'freeze_layers_reversed'
+        ]
+        # extract the parameters from the kwargs
+        train_param = {k: v for k, v in kwargs.items() 
+                       if k in unimol_mol_train_instantiate_selected_kwargs}
         pred = MolTrain(task='multilabel_regression',
                         data_type='molecule',
                         epochs=epochs,
@@ -79,7 +123,8 @@ class UniMolRegressionNet:
                         model_name=self.model_name,
                         model_size=self.model_size,
                         save_path=save_path,
-                        load_model_dir=self.model_restart_from).fit(data=data)
+                        load_model_dir=self.model_restart_from,
+                        **train_param).fit(data=data)
 
         # cache the model trained
         self.model = MolPredict(load_model=save_path)        
